7e8e5db6f9fdd4bd1b8f89e91c5cba670257b433
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-09-23 12:57:10 -0300

    update

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt update && apt install -y wget unzip zip
+wget -qq $(wget -qO- https://www.minecraft.net/en-us/download/server/bedrock | grep serverBedrockLinux | sed 's| class="btn btn-disabled-outline mt-4 downloadlink" role="button" data-platform="serverBedrockLinux">Download </a>||g' | sed 's|<a href=||g' | sed 's| ||g' | sed 's|"||g') -O /tmp/bds.zip
+echo "wget status: $?"
+unzip -o /tmp/bds.zip -d opt/BDS-Common/
+cd opt/*/
+rm -rf server.properties worlds
+ln -s /etc/BDS-Common/server.properties server.properties
+ln -s /etc/BDS-Common/worlds worlds
