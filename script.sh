d7427523c0d4bdb7da914cb6c0b85725f8985176
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-09-26 23:01:31 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt update && apt install -y wget unzip zip
-wget -qq $(wget -qO- https://www.minecraft.net/en-us/download/server/bedrock | grep bin-linux | sed 's| class="btn btn-disabled-outline mt-4 downloadlink" role="button" data-platform="serverBedrockLinux" tabindex="-1">Download </a>||g' | sed 's|<a href=||g' | sed 's| ||g' | sed 's|"||g') -O /tmp/bds.zip
+apt update && apt install -y wget unzip zip lynx
+wget -qq $(lynx -listonly -nonumbers -dump https://www.minecraft.net/en-us/download/server/bedrock | grep bin-linux) -O /tmp/bds.zip
 echo "wget status: $?"
 unzip -o /tmp/bds.zip -d opt/BDS-Common/
 cd opt/*/
