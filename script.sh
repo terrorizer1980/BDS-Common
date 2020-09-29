2d2cad9d42211d2c7af16636cfc968ec5f7b2311
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-09-28 23:39:34 -0300

    teste

--- script.sh
+++ script.sh
@@ -2,7 +2,10 @@
 apt update && apt install -y wget unzip zip lynx
 wget -qq $(lynx -listonly -nonumbers -dump https://www.minecraft.net/en-us/download/server/bedrock | grep bin-linux) -O /tmp/bds.zip
 echo "wget status: $?"
-unzip -o /tmp/bds.zip -d opt/BDS-Common/
+unzip -oqq /tmp/bds.zip -d opt/BDS-Common/
+VERSS="$(lynx -listonly -nonumbers -dump https://www.minecraft.net/en-us/download/server/bedrock | grep bin-linux | sed 's|https://minecraft.azureedge.net/bin-linux/bedrock-server-||g' | sed 's|.zip||g')"
+OLDVER="$(cat */control | grep 'Version:')"
+sed -i "s|$OLDVER|Version: $VERSS|g"
 cd opt/*/
 rm -rf server.properties worlds permissions.json whitelist.json placehold
 ln -s /etc/BDS-Common/server.properties server.properties
