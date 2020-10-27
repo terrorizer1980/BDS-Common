48178070990339a80006bf83a2f1aa6bae1b54a1
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-10-26 22:21:22 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-apt update && apt install -y wget unzip zip lynx
+apt update && apt install -y wget unzip zip lynx &> /dev/null
 wget -qq $(lynx -listonly -nonumbers -dump https://www.minecraft.net/en-us/download/server/bedrock | grep bin-linux) -O /tmp/bds.zip
 echo "wget status: $?"
 unzip -oqq /tmp/bds.zip -d opt/BDS-Common/
 echo "unzip status: $?"
-VERSS="$(lynx -listonly -nonumbers -dump https://www.minecraft.net/en-us/download/server/bedrock |grep 'linux'|sed 's|.zip||g'|sed 's|/bin-linux/||g'|sed 's|https://||g'|sed 's|minecraft.azureedge.net||g')"
+VERSS="$(lynx -listonly -nonumbers -dump https://www.minecraft.net/en-us/download/server/bedrock |grep 'linux'|sed 's|.zip||g'|sed 's|/bin-linux/||g'|sed 's|https://||g'|sed 's|minecraft.azureedge.net||g'|sed 's|bedrock-server-||g')"
 OLDVER="$(cat */control | grep 'Version:')"
 sed -i "s|$OLDVER|Version: $VERSS|g" */control
 cd opt/*/
