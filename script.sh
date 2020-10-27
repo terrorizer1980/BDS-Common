9166e34264009156e3a0ea15dff8eddbf38e8be8
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-10-26 22:17:13 -0300

    push

--- script.sh
+++ script.sh
@@ -3,7 +3,8 @@ apt update && apt install -y wget unzip zip lynx
 wget -qq $(lynx -listonly -nonumbers -dump https://www.minecraft.net/en-us/download/server/bedrock | grep bin-linux) -O /tmp/bds.zip
 echo "wget status: $?"
 unzip -oqq /tmp/bds.zip -d opt/BDS-Common/
-VERSS="$(lynx -listonly -nonumbers -dump https://www.minecraft.net/en-us/download/server/bedrock | grep bin-linux | sed 's|https://minecraft.azureedge.net/bin-linux/bedrock-server-||g' | sed 's|.zip||g')"
+echo "unzip status: $?"
+VERSS="$(lynx -listonly -nonumbers -dump https://www.minecraft.net/en-us/download/server/bedrock |grep 'linux'|sed 's|.zip||g'|sed 's|/bin-linux/||g'|sed 's|https://||g'|sed 's|minecraft.azureedge.net||g')"
 OLDVER="$(cat */control | grep 'Version:')"
 sed -i "s|$OLDVER|Version: $VERSS|g" */control
 cd opt/*/
@@ -12,3 +13,5 @@ ln -s /etc/BDS-Common/server.properties server.properties
 ln -s /etc/BDS-Common/worlds worlds
 ln -s /etc/BDS-Common/permissions.json permissions.json
 ln -s /etc/BDS-Common/whitelist.json whitelist.json
+
+echo;echo;echo;echo;echo;echo;echo;
